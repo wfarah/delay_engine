5227be7787283a0d57260c7bb748ce72364cf1cf
RocketRoss <code@radonn.co.za>
2021-11-24 01:26:25 +0000

    add antenna names to read_vis plots

--- read_vis.py
+++ read_vis.py
@@ -10,15 +10,22 @@ NPOLS   = 2    # number of polarisations per block
 NANTS   = 20   # number of antennas per block
 NFFT    = 1    # number of FFTs we want to take. 0.5/4 = 125 kHz channel width
 
+antnames = "1cB,1eB,1gB,1hB,1kB,2aB,2bB,2cB,2eB,2hB,2jB,2kB,2lB,2mB,3cB,3dB,3lB,4jB,5bB,4gB"
+antnames = antnames.split(",")
+
 vis_name = sys.argv[1]
 
+#vis = np.fromfile(vis_name, dtype=np.complex64).\
+#        reshape((NFILES*NBLOCKS, NANTS, NCHANS*NFFT, NPOLS*NPOLS))
 vis = np.fromfile(vis_name, dtype=np.complex64).\
-        reshape((NFILES*NBLOCKS, NANTS, NCHANS*NFFT, NPOLS*NPOLS))
+        reshape((-1, NANTS, NCHANS*NFFT, NPOLS*NPOLS))
 
 for iant in range(NANTS):
     plt.figure(2*iant)
     plt.plot(np.angle(vis[:, iant, :, 0].sum(axis=0)), ".")
+    plt.title(antnames[iant])
     plt.figure(2*iant+1)
     plt.plot(np.abs(np.fft.ifftshift(np.fft.ifft(vis[:, iant, :, 0].sum(axis=0)))))
+    plt.title(antnames[iant])
 
 plt.show()
